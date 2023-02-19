c5566d72013b4e7be187c3f0d8316a4c57a6e64a
Pablo Stefan Gomes Pereira <pablo.stefan744@gmail.com>
2023-02-19 12:17:40 -0300

    todo

--- module_welcome/.ios/Flutter/flutter_export_environment.sh
+++ module_welcome/.ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/krykto/Documents/todo_app/module_welcome"
+export "FLUTTER_APPLICATION_PATH=/Users/krykto/Documents/GitHub/todo_app/module_welcome"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
